94b9573a62a8b4fc871fac625fc530a7b7e0cd9f
Kim Tore Jensen <kimtjen@gmail.com>
2018-03-22 13:56:41 +0100

    Produce less output by grouping violations by file.

--- truffletool.go
+++ truffletool.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	flag "github.com/spf13/pflag"
 )
@@ -25,21 +26,64 @@ type Truffle struct {
 	StringsFound []string
 }
 
-func (t *Truffle) Print() {
-	fmt.Printf("\n")
-	fmt.Printf("**************************************\n")
-	fmt.Printf("*** %s\n", t.Reason)
-	fmt.Printf("**************************************\n")
-	fmt.Printf("[*] commit......: %s\n", t.CommitHash)
-	fmt.Printf("[*] branch......: %s\n", t.Branch)
-	fmt.Printf("[*] file........: %s\n", t.Path)
-	fmt.Printf("[*] violations..: %d\n", len(t.StringsFound))
+type Violation struct {
+	File     string
+	Branches map[string]int
+	Commits  map[string]int
+	Strings  map[string]int
+	Reasons  map[string]int
+}
+
+func NewViolation(file string) Violation {
+	return Violation{
+		File:     file,
+		Branches: map[string]int{},
+		Commits:  map[string]int{},
+		Reasons:  map[string]int{},
+		Strings:  map[string]int{},
+	}
+}
+
+func (v Violation) AddTruffle(t Truffle) Violation {
+	v.Commits[t.CommitHash] += 1
+	v.Branches[t.Branch] += 1
+	v.Reasons[t.Reason] += 1
 	for _, s := range t.StringsFound {
+		v.Strings[s] += 1
+	}
+	return v
+}
+
+func commitDigest(hash map[string]int) string {
+	for k, _ := range hash {
+		if len(hash) == 1 {
+			return k
+		}
+		return fmt.Sprintf("%s (and %d more)", k, len(hash)-1)
+	}
+	return ""
+}
+
+func digest(hash map[string]int) string {
+	s := make([]string, 0, len(hash))
+	for k, v := range hash {
+		s = append(s, fmt.Sprintf("%s (%d)", k, v))
+	}
+	return strings.Join(s, ", ")
+}
+
+func (v *Violation) Print() {
+	fmt.Printf("[*] file.....: %s\n", v.File)
+	fmt.Printf("[*] commits..: %s\n", commitDigest(v.Commits))
+	fmt.Printf("[*] branches.: %s\n", digest(v.Branches))
+	fmt.Printf("[*] reasons..: %s\n", digest(v.Reasons))
+	for s, _ := range v.Strings {
 		if len(s) > 80 {
 			s = s[:80] + fmt.Sprintf("...(%d characters truncated)", len(s)-80)
 		}
 		fmt.Println(s)
 	}
+	fmt.Printf("---\n")
 }
 
 func excluded(t Truffle) bool {
@@ -54,6 +98,9 @@ func excluded(t Truffle) bool {
 
 func main() {
 	flag.Parse()
+
+	// store violations
+	violations := map[string]Violation{}
 
 	// decode json from standard input
 	reader := bufio.NewReader(os.Stdin)
@@ -71,6 +118,14 @@ func main() {
 			continue
 		}
 
-		t.Print()
+		v, ok := violations[t.Path]
+		if !ok {
+			v = NewViolation(t.Path)
+		}
+		violations[t.Path] = v.AddTruffle(t)
+	}
+
+	for _, v := range violations {
+		v.Print()
 	}
 }
